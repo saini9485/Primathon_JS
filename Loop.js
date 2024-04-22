aeb7003a57072a6e68bad53a283c647167b8f45c
Rajesh Saini <saini222302@gmail.com>
2024-04-22 20:30:28 +0530

    Loop

---
+++ Loop.js
@@ -0,0 +1,53 @@
+1.
+// let count = 1;
+// while (count < 10) {
+//  console.log(count);
+//  count +=2;
+// }
+// 1,3,5,7,9
+
+2.
+// let count = 1;
+// while (count < 10) {
+//  console.log("Hello World");
+//  count += 4;
+//} 
+// Hello World
+//   Hello World
+//   Hello World
+
+3.
+// const ourArray = [];
+// let i = 0;
+// while (i < 5 ) 
+// {
+// ourArray.push(i);
+// i++ ;
+// };
+// console.log(ourArray); // [ 0, 1, 2, 3, 4 ]
+
+4. 
+// let count = 0;
+// do {
+//  console.log(count);
+//  count++;
+// } while (count < 5)  // 0,1,2,3,4
+
+5.
+// let count = 3;
+// do {
+//  console.log(count);
+//  count++;
+// } while (count < 4) // 3
+
+6.
+// const ourArray = [];
+// let i = 0;
+// do {
+// ourArray.push(i);
+// i++ ;
+// }
+//  while (i < 5 );
+// console.log(ourArray); // [ 0, 1, 2, 3, 4 ]
+
+
