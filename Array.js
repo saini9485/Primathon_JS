6e8d89a232ba64a7622c733ec94475bf4ba920d2
Rajesh Saini <saini222302@gmail.com>
2024-04-25 23:36:06 +0530

    Array

--- Array.js
+++ Array.js
@@ -1,24 +1,43 @@
 //Array method 
 // 1. push() >>  add  the element in the end of array and return new array 
-
 // let arr = [1,2,3,4,5,6]
 // arr.push(7,8)
 // console.log(arr)
 
-// 2. pop()
+// 2. pop() >> use for delete the last array 
 // let arr = [1,2,3,4,5,6,7]
 // arr.pop()
 // console.log(arr)
 
-// 3. shift()
-let arr = [1,2,3,4,5,6,7]
-arr.pop()
-console.log(arr)
+// 3. shift() >>> use for  delete the  array in first 
+// let arr = [1,2,3,4,5,6,7]
+// arr.shift()
+// console.log(arr) // [ 2, 3, 4, 5, 6, 7 ]
 
-// 4. unshift()
-// 5. filter()
-// 6. map()
-// 7. reduce()
+// 4. unshift() >>> add the array in first 
+//  let arr = [1,2,3,4,5,6,7]
+//  arr.unshift(0)
+//  console.log(arr) // [0, 2, 3, 4, 5, 6, 7 ]
+
+// 5. map() // map return new array after applying the condition
+//  let arr = [1,2,3,4,5,6]
+//  let res = arr.map((num)=>num*2)
+//  console.log(res) // [ 2, 4, 6, 8, 10, 12 ]
+
+// 6. filter() // filter element return new array whose fulfil the condition
+// let arr = [1,2,3,4,5,6]
+//  let res = arr.filter((num)=>num>5)
+//  console.log(res)  // [ 6 ]
+
+// 7. reduce() // 
+let arr = [1,2,3,4,5]
+let sum = arr.reduce((acc,  curr ) => {
+   return  acc + curr
+ 
+},8)
+console.log(sum)
+
+
 // 8. forEach()
 // 9. concat()
 // 10. flat()
@@ -32,5 +51,6 @@ console.log(arr)
 // 18. sort()
 // 19. toString()
 // 20. every()
+// 21 includes()
 
 
