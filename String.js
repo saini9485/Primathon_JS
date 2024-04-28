f5e141d1f7b8e478dd9d7cc8837824b5b70ad018
Rajesh Saini <saini222302@gmail.com>
2024-04-29 04:25:25 +0530

    String Methods

--- String.js
+++ String.js
@@ -1,18 +1,33 @@
 // String Method
 // 1. charAt() > 
-let str = "Primathon Academy" 
-let p = str.charAt(5)
-console.log(p)
+// let str = "Primathon Academy" 
+// let p = str.charAt(5)
+// console.log(p)
 // 2. slice(start, end) > Extracting parts from a string 
+// let str = "Primathon Academy" 
+// let res = str.slice(0, 5)
+// console.log(res)
 // 3. substring(start, end) >
 // 4. substr(start, length) >
 // 5. concat() >
 // 6. includes() >
+// let str = "Primathon Academy" 
+// let res = str.includes( "P")
+// console.log(res) // true
 // 7. indexOf() >
-// 8. repeat() >
+// 8. repeat() > 
+let str = "  Primathon Academy" 
+let res = str.repeat(3)  
+console.log(res)
 // 9. replace() >
+// let str = "  Primathon Academy" 
+// let res = str.replace( "i", "R")  
+// console.log(res)
 //10. search() >
 //11. toLowerCase()  >
 //12. toUpperCase() >
 //13. trim() >
+// let str = "  Primathon Academy" 
+// let res = str.trim()  
+// console.log(res)
 //14. replaceAll() >
